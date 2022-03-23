e5cbb7a813b87912d1020603b0bba765737cc196
Salomon Metre <kulosmetros088@mgmail.com>
2022-03-23 13:37:02 +0300

    First Flutter app

---
+++ README.md
@@ -0,0 +1,47 @@
+# hello_flutter
+
+A new Flutter project.
+
+# What did I learn?
+
+# Basics of Dart
+    Variables
+    Type inference
+    Conditional Statements
+    Iterative statements (loops)
+    Branching statements
+    functions
+    Arrow syntax
+    parameters and types of parameters
+    classes
+    Lists, generics and list methods
+    String and special string methods
+    String interpolation
+    Classes and objects
+
+# Widgets
+    Stateless Widget vs Stateful Widgets
+    MaterialApp
+        title
+        home
+    Scaffold
+        appBar
+        body
+    Image.
+        network
+        file
+        memory
+        asset
+    AlertDialog
+    Padding
+    Margin
+    EdgeInsets
+    SingleChildScrollView
+
+# Calling special methods
+    showDialog
+
+
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'dart:developer' show log;
+import 'package:flutter/material.dart';
+
+num addNumbers(num a, num b) => a + b;
+
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Hello World App',
+    home: HomeView(),
+  ));
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Travel Hello World'),
+        centerTitle: true,
+      ),
+      body: const Body(),
+    );
+  }
+}
+
+class Body extends StatefulWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  static const imageLink =
+      'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg';
+
+  onDialogClose() {
+    Navigator.pop(context);
+  }
+
+  onContactUs() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Contact Us'),
+            content: const Text('Email : salomon.kulondwa@gmail.com'),
+            actions: [
+              TextButton(
+                child: const Text('Close'),
+                onPressed: onDialogClose,
+              )
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.black,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.network(
+            imageLink,
+            height: 350,
+          ),
+          ElevatedButton(
+            child: const Text('Contact Us !'),
+            onPressed: onContactUs,
+          )
+        ],
+      ),
+    );
+  }
+}
